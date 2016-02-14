5c0b8e916d40d965d46472f90d7405c665222f5e
Michael L Perry <michael@qedcode.com>
2016-02-14 12:18:28 -0600

    Added distributor to web app.

---
+++ Commuter.Web/Controllers/DistributorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace Commuter.Web.Controllers
+{
+    public class DistributorController : RoverMob.Distributor.Controllers.DistributorController
+    {
+        protected DistributorController() : base(
+            WebConfigurationManager.AppSettings["StorageConnectionString"],
+            WebConfigurationManager.AppSettings["NotificationConnectionString"],
+            WebConfigurationManager.AppSettings["ServiceBusConnectionString"],
+            WebConfigurationManager.AppSettings["ServiceBusPath"])
+        {
+
+        }
+
+        protected override Task<bool> AuthorizeUserForGet(string topic, string userId)
+        {
+            return Task.FromResult(true);
+        }
+
+        protected override Task<bool> AuthorizeUserForPost(string topic, string userId)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
