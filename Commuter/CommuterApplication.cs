4f8320806e3084107e8de4413cdec6b68be25c00
Michael L Perry <michael@qedcode.com>
2016-04-09 23:51:53 -0500

    Create design mode data for MyCommute.

--- Commuter/CommuterApplication.cs
+++ Commuter/CommuterApplication.cs
@@ -42,6 +42,30 @@ namespace Commuter
         {
             CommuterApplication application = new CommuterApplication();
             application.Load(new User(Guid.NewGuid()));
+
+            application.EmitMessage(Message.CreateMessage(
+                application.Root.GetObjectId().ToCanonicalString(),
+                "Queue",
+                application.Root.GetObjectId(),
+                new
+                {
+                    Title = "QED 12: Difference Engine",
+                    Summary = @"The Difference Engine was a mechanical computer that could calculate tables of numbers based on polynomials. The amazing thing is, though, that it could only add. How then could it accomplish this feat? By the method of differences! Charles Babbage never constructed his Difference Engine, but we've made a couple from his designs.
+Lambda Calculus is also a method of computation based on really simple rules. In this case, they are alpha-conversion, beta-conversion, and eta-conversion. These serious-sounding transforms are actually pretty simple. Let's first learn what they are, and then see how they relate to C#.
+Speaking of C#, Malachi asks a question about C# constructors. As you may know, I am of the opinion that constructor parameters should represent only immutable fields. How, then, does one initialize a mutable field in such a way that you can guarantee that it is set? In other words, write a method on the class that can only be called after the object is initialized.",
+                    PublishedDate = DateTime.Parse("7/19/2015"),
+                    MediaUrl = "http://traffic.libsyn.com/qedcode/qed12-DifferenceEngine.mp3",
+                    ImageUri = "http://assets.libsyn.com/content/7569532"
+                }));
+            application.EmitMessage(Message.CreateMessage(
+                null,
+                "Downloaded",
+                new { UserGuid = application.Root.GetObjectId(), mediaUrl = "http://traffic.libsyn.com/qedcode/qed12-DifferenceEngine.mp3" }.ToGuid(),
+                new
+                {
+                    FileName = @"c:\Fake\File\Path.mp3"
+                }));
+
             return application;
         }
 
